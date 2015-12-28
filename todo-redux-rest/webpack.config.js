9cc2d4ee353a4ba7ff5a04fff8c545dc5ac6c135
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2015-12-28 17:15:31 -0600

    added some comments

--- todo-redux-rest/webpack.config.js
+++ todo-redux-rest/webpack.config.js
@@ -4,8 +4,9 @@ const publicPath = path.join(__dirname, 'public');
 module.exports = {
   entry: path.join(publicPath, 'todo-app.js'),
   output: {
-    path: publicPath,
-    filename: 'build/bundle.js'
+    //path: __dirname, filename: 'public/build/bundle.js DOES NOT WORK!
+    //path: __dirname + '/public/build', filename: 'bundle.js DOES NOT WORK!
+    path: publicPath, filename: 'build/bundle.js' // THIS WORKS!
   },
   module: {
     loaders: [
