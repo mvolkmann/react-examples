f2ca26763b396550ad70a9eddeed3677c855a53d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-01-03 17:16:12 -0600

    added more Immutable examples

--- Immutable/demo.js
+++ Immutable/demo.js
@@ -1,5 +1,30 @@
 import Immutable from 'immutable';
 
+let person = Immutable.fromJS({
+  name: 'Moe Howard',
+  address: {
+    street: '123 Some Street',
+    city: 'Somewhere',
+    state: 'MO',
+    zip: 12345
+  }
+});
+
+/*
+person = person.set('name', 'Larry Fine');
+person = person.setIn(['address', 'city'], 'Los Angeles');
+console.log('name =', person.get('name'));
+console.log('city =', person.getIn(['address', 'city']));
+person = person.deleteIn(['address', 'street']);
+person = person.updateIn(['address', 'zip'], zip => zip + 1);
+*/
+person = person.withMutations(mutPerson =>
+  mutPerson.set('name', 'Larry Fine').
+    setIn(['address', 'city'], 'Los Angeles').
+    deleteIn(['address', 'street']).
+    updateIn(['address', 'zip'], zip => zip + 1));
+console.log(person.toJS());
+
 const balls = [
   {sport: 'baseball', color: 'white', size: 'medium'},
   {sport: 'basketball', color: 'orange', size: 'large'},
@@ -15,12 +40,21 @@ const ballMap = Immutable.Map().withMutations(map => {
   }
 });
 
+const iter = Immutable.Iterable(['Moe', 'Larry', 'Curly']);
+for (const stooge of iter) {
+  console.log('stooge =', stooge);
+}
+
+for (const sport of ballMap.keys()) {
+  console.log('sport =', sport);
+}
+
 const groups = ballMap.groupBy(ball => ball.size);
 console.log('demo.js: groups =', JSON.stringify(groups));
 
 for (const size of groups.keys()) {
-  console.log('size:', size)
+  console.log('size:', size);
   for (const [sport, ball] of groups.get(size)) {
-    console.log('  sport:', sport);
+    console.log(sport, 'uses', ball);
   }
 }
