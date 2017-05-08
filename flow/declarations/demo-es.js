5fd7a113bc2b71e68836a33b70a18a79f89c0e5e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-05-08 16:23:05 -0500

    more work on .js.flow files

--- flow/declarations/demo-es.js
+++ flow/declarations/demo-es.js
@@ -1,5 +1,5 @@
 // @flow
 import * as math from './math';
 
-console.log(math.double(3)); // good
-//console.log(math.double('bad')); // error
+//console.log(math.double(3)); // good
+console.log(math.double('bad')); // error
