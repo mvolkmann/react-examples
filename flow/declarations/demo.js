63e83e5851aec772f934b7d38cf3d42cc3287cb7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-05-08 07:08:53 -0700

    added untyped.ts file

--- flow/declarations/demo.js
+++ flow/declarations/demo.js
@@ -1,5 +1,5 @@
 // @flow
 const math = require('./math');
 
-console.log(math.double(3)); // good
-//console.log(math.double('bad')); // error
+//console.log(math.double(3)); // good
+console.log(math.double('bad')); // error
---
+++ flow/src/untyped.ts
@@ -0,0 +1,8 @@
+// @flow
+
+function rectangleArea(width, height) {
+  return width * height;
+}
+
+const area = rectangleArea(100, '50');
+console.log('area =', area);
