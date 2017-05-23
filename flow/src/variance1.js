746c1a02d14dbc48280b4f67b864e054c601d683
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-05-22 19:35:32 -0500

    added more to variance1.js

--- flow/src/variance1.js
+++ flow/src/variance1.js
@@ -1,5 +1,8 @@
 // @flow
 
+// TypeScript relies on duck typing rather than the class hierarchy.
+// It views all these classes as equivalent because
+// they do not differ in properties or methods.
 class LivingThing {}
 class Plant extends LivingThing {}
 class Animal extends LivingThing {}
@@ -10,14 +13,19 @@ const animals: Animal[] = [];
 // In Flow, adding elements to arrays is covariant (can add subtypes).
 animals.push(new Dog());
 animals.push(new Cat());
-animals.push(new Plant()); // error in Flow, but not TS
+//animals.push(new Plant()); // error in Flow, but not TS
 
 // In Flow, extracting elements from arrays is contravariant
 // (get type of array which is the supertype of the elements)
-// eslint-disable prefer-destructuring
-const dog: Dog = animals[0]; // error in Flow, but not TS
-//const dog: Animal = animals[0]; // works
+//const dog: Dog = animals[0]; // error in Flow, but not TS
+
+/* eslint-disable prefer-destructuring */
+const dog: Animal = animals[0]; // works
 console.log('dog =', dog);
+
+// Typecasting to any and then to Dog works.
+const dogTypecast: Dog = ((animals[0]: any): Dog);
+console.log('dogTypecast =', dogTypecast);
 
 const plant: Plant = animals[0]; // error in Flow, but not TS
 console.log('plant =', plant);
