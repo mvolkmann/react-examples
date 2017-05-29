31deef4ee1f15a0878d2d638cf721f9b25ccbbcd
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-05-29 10:03:10 -0500

    updated flow/src/sealed-objects.js

--- flow/src/sealed-objects.js
+++ flow/src/sealed-objects.js
@@ -2,9 +2,11 @@
 
 // Sealed object
 const obj1 = {foo: 1};
-obj1.bar = 2; // flow error
+obj1.bar = 2; // error
+obj1.foo = 2; // okay
+obj1.foo = 'test'; // error
 
 // Unsealed object
 const obj2 = {};
-obj2.foo = 1;
-obj2.bar = 2;
+obj2.foo = 1; // okay
+obj2.bar = 'test'; // okay
